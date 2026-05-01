5b0fef40e9aa16a3cd054c9cfa98fa37a0e66824
Kevin Boudreault <kevinboudreault@gmail.com>
2026-05-01 12:05:44 -0400

    Update extract_env.zsh

--- .config/zsh/custom/extract_env.zsh
+++ .config/zsh/custom/extract_env.zsh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+ 
 extract_env() {
     # Check if an argument is provided and if the file exists
     if [[ -z "$1" ]]; then
@@ -8,8 +10,9 @@ extract_env() {
         return 1
     fi
 
+    # Determine destination: $2 if provided, otherwise standard output
+    local dest="${2:-/dev/stdout}"
+
     # Extract keys using yq
-    yq eval '.services[].environment[]' "$1" | cut -d '=' -f 1 | sort -u
+    yq eval '.services[].environment[]' "$1" | cut -d '=' -f 1 | sort -u > "$dest"
 }
-
-
